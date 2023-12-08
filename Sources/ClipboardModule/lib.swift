6ba4d760606b606a4556a9658b3b07cc3721c427
citrus-lemon <lihanyuan1996@gmail.com>
2023-12-08 11:55:26 +0900

    add readme description

--- Sources/ClipboardModule/lib.swift
+++ Sources/ClipboardModule/lib.swift
@@ -15,16 +15,48 @@ func extractPasteboard() -> List<List<ConsCell<String, DataString?>>>? {
     }
 }
 
+func extractPasteboardWithTypes(_ types: List<String>) -> List<List<ConsCell<String, DataString>>>? {
+    if let result = NSPasteboard.general.pasteboardItems?.map({ item in
+        List(from: types.compactMap { type in (
+            item.string(forType: NSPasteboard.PasteboardType(type)).map(DataString.String) ??
+            item.data(forType: NSPasteboard.PasteboardType(type)).map(DataString.Data)).map {
+                ConsCell(car: type, cdr: $0)}})
+    }) {
+        List(from: result)
+    } else {
+        nil
+    }
+}
+
+func extractPasteboardOnlyTypes() -> List<List<String>>? {(
+    NSPasteboard.general.pasteboardItems?.map {
+        item in List(from: item.types.map {$0.rawValue} )
+    }).map { List(from: $0) }
+}
+
+func clearPasteboard() {
+    NSPasteboard.general.clearContents()
+}
+
 func setStringPasteboard(str: String, type: String?) {
     let pasteboardType = NSPasteboard.PasteboardType(type ?? "public.utf8-plain-text")
     NSPasteboard.general.setString(str, forType: pasteboardType)
 }
 
+func setDataPasteboard(data: Data, type: String?) {
+    let pasteboardType = NSPasteboard.PasteboardType(type ?? "public.utf8-plain-text")
+    NSPasteboard.general.setData(data, forType: pasteboardType)
+}
+
 class ClipboardModule: Module {
     let isGPLCompatible = true
     func Init(_ env: Environment) throws {
-        try env.defun("macos-clipboard-extract-pasteboard", with: "", function: extractPasteboard)
-        try env.defun("macos-clipboard-set-string", with: "", function: setStringPasteboard)
+        try env.defun("macos-clipboard--extract-pasteboard", with: "", function: extractPasteboard)
+        try env.defun("macos-clipboard--extract-pasteboard-with-types", with: "", function: extractPasteboardWithTypes)
+        try env.defun("macos-clipboard--extract-pasteboard-only-types", with: "", function: extractPasteboardOnlyTypes)
+        try env.defun("macos-clipboard--set-string", with: "", function: setStringPasteboard)
+        try env.defun("macos-clipboard--set-data", with: "", function: setDataPasteboard)
+        try env.defun("macos-clipboard--clearPasteboard", with: "", function: clearPasteboard)
         try env.funcall("provide", with: Symbol(name: "macos-clipboard-nspasteboard"))
     }
 }
--- macos-clipboard.el
+++ macos-clipboard.el
@@ -4,5 +4,15 @@
 
 (require 'macos-clipboard-nspasteboard)
 
+(defun macos-clipboard-extract-pasteboard (&optional types)
+  (if types
+      (if (eq types 'only-types)
+          (macos-clipboard--extract-pasteboard-only-types)
+        (macos-clipboard--extract-pasteboard-with-types (if (listp types) types (list types))))
+    (macos-clipboard--extract-pasteboard)))
+
+(defun macos-clipboard-set-string (value &optional type)
+  (macos-clipboard--set-string value type))
+
 (provide 'macos-clipboard)
 ;;; macos-clipboard.el ends here
